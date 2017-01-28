54d277134e1dd34ba0f7410e2ea7c671329c057c
ofirl <ofirl24@gmail.com>
2017-01-28 21:52:46 +0200

    testing

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -13,63 +13,70 @@ import static spark.Spark.get;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    port(Integer.valueOf(System.getenv("PORT")));
-    staticFileLocation("/public");
+        port(Integer.valueOf(System.getenv("PORT")));
+        staticFileLocation("/public");
 
-    get("/hello", (req, res) -> {
-          return "hi!";
+        get("/hello", (req, res) -> {
+            return "hi!";
         });
 
-    get("/", (request, response) -> {
+        get("/", (request, response) -> {
             Map<String, Object> attributes = new HashMap<>();
             attributes.put("message", "Hello World!");
 
             return new ModelAndView(attributes, "index.ftl");
         }, new FreeMarkerEngine());
 
-    get("/db", (req, res) -> {
+        get("/db", (req, res) -> {
   /*private void showDatabase(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException { */
-      Connection connection = null;
-      Map<String, Object> attributes = new HashMap<>();
-      try {
-        connection = getConnection();
+            Connection connection = null;
 
-        Statement stmt = connection.createStatement();
-        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
-        stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
-        ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
+            Map<String, Object> attributes = new HashMap<>();
 
-        ArrayList<String> output = new ArrayList<String>();
-        while (rs.next()) {
-          output.add( "Read from DB: " + rs.getTimestamp("tick"));
-        }
+            // testing
+            attributes.put("message", res);
+            return new ModelAndView("", "error.ftl");
+            // end testing
 
-        attributes.put("results", output);
-        return new ModelAndView(attributes, "db.ftl");
-      } catch (Exception e) {
-        attributes.put("message", "There was an error: " + e);
-        return new ModelAndView(attributes, "error.ftl");
-      } finally {
-        if (connection != null) try{connection.close();} catch(SQLException e){}
-      }
-  }, new FreeMarkerEngine());
+            try {
+                connection = getConnection();
 
-  }
+                Statement stmt = connection.createStatement();
+                stmt.executeUpdate("CREATE TABLE IF NOT EXISTS ticks (tick timestamp)");
+                stmt.executeUpdate("INSERT INTO ticks VALUES (now())");
+                ResultSet rs = stmt.executeQuery("SELECT tick FROM ticks");
 
-  private static Connection getConnection() throws URISyntaxException, SQLException {
-    URI dbUri = new URI(System.getenv("DATABASE_URL"));
-    int port = dbUri.getPort();
-    String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+                ArrayList<String> output = new ArrayList<String>();
+                while (rs.next()) {
+                    output.add( "Read from DB: " + rs.getTimestamp("tick"));
+                }
 
-    if (dbUri.getUserInfo() != null) {
-      String username = dbUri.getUserInfo().split(":")[0];
-      String password = dbUri.getUserInfo().split(":")[1];
-      return DriverManager.getConnection(dbUrl, username, password);
-    } else {
-      return DriverManager.getConnection(dbUrl);
+                attributes.put("results", output);
+                return new ModelAndView(attributes, "db.ftl");
+            } catch (Exception e) {
+                attributes.put("message", "There was an error: " + e);
+                return new ModelAndView(attributes, "error.ftl");
+            } finally {
+                if (connection != null) try{connection.close();} catch(SQLException e){}
+            }
+        }, new FreeMarkerEngine());
+
     }
-  }
+
+    private static Connection getConnection() throws URISyntaxException, SQLException {
+        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+        int port = dbUri.getPort();
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ":" + port + dbUri.getPath();
+
+        if (dbUri.getUserInfo() != null) {
+            String username = dbUri.getUserInfo().split(":")[0];
+            String password = dbUri.getUserInfo().split(":")[1];
+            return DriverManager.getConnection(dbUrl, username, password);
+        } else {
+            return DriverManager.getConnection(dbUrl);
+        }
+    }
 }
