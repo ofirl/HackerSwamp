c32efd6f39fad7978587869eb514ee930f64a694
ofirl <ofirl24@gmail.com>
2017-02-04 19:55:06 +0200

    added unit testing sample

---
+++ src/main/java/processes/WorkerTest.java
@@ -0,0 +1,51 @@
+package processes;
+
+import static org.junit.Assert.*;
+
+public class WorkerTest {
+    @org.junit.Before
+    public void setUp() throws Exception {
+        //Worker.initializeCommands();
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void main() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void initializeCommands() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void workerStart() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void getAccessibleCommands() throws Exception {
+        if (Worker.allCommands.size() != 1) throw new AssertionError("test");
+    }
+
+    @org.junit.Test
+    public void getAccessibleCommands1() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void parseCommand() throws Exception {
+
+    }
+
+    @org.junit.Test
+    public void checkSyntax() throws Exception {
+
+    }
+
+}
