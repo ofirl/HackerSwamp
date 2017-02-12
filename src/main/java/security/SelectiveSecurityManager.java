f6b065ddc9d8be01df01337e489811db84fdd0cb
ofirl <ofirl24@gmail.com>
2017-02-12 23:35:45 +0200

    added template for security manager

---
+++ src/main/java/security/SelectiveSecurityManager.java
@@ -0,0 +1,60 @@
+package security;
+
+import java.security.Permission;
+
+public class SelectiveSecurityManager extends SecurityManager {
+
+    private static final ToggleSecurityManagerPermission TOGGLE_PERMISSION = new ToggleSecurityManagerPermission();
+
+    ThreadLocal<Boolean> enabledFlag = null;
+
+    public SelectiveSecurityManager(final boolean enabledByDefault) {
+
+        enabledFlag = new ThreadLocal<Boolean>() {
+
+            @Override
+            protected Boolean initialValue() {
+                return enabledByDefault;
+            }
+
+            @Override
+            public void set(Boolean value) {
+                SecurityManager securityManager = System.getSecurityManager();
+                if (securityManager != null) {
+                    securityManager.checkPermission(TOGGLE_PERMISSION);
+                }
+                super.set(value);
+            }
+        };
+    }
+
+    @Override
+    public void checkPermission(Permission permission) {
+        if (shouldCheck(permission)) {
+            super.checkPermission(permission);
+        }
+    }
+
+    @Override
+    public void checkPermission(Permission permission, Object context) {
+        if (shouldCheck(permission)) {
+            super.checkPermission(permission, context);
+        }
+    }
+
+    private boolean shouldCheck(Permission permission) {
+        return isEnabled() || permission instanceof ToggleSecurityManagerPermission;
+    }
+
+    public void enable() {
+        enabledFlag.set(true);
+    }
+
+    public void disable() {
+        enabledFlag.set(false);
+    }
+
+    public boolean isEnabled() {
+        return enabledFlag.get();
+    }
+}
