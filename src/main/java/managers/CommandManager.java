6501fbf491509731a63792f4adbd22f6f816d4e5
ofirl <ofirl24@gmail.com>
2017-02-17 16:28:18 +0200

    testing

--- src/main/java/managers/CommandManager.java
+++ src/main/java/managers/CommandManager.java
@@ -57,6 +57,7 @@ public class CommandManager {
         cmd = addSystemCommand(commandIds.get(name), name, new Help(), true);
         // help.commands
         name = Parameters.CommandNameHelpCommands;
+        Logger.log("CommandManager.initSystemCommands", commandIds.get(name).toString());
         subCmd = addSystemCommand(commandIds.get(name), name, new Help(), false);
         addSubCommand(cmd, subCmd);
         // connect
