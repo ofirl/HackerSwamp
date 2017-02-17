3cb5d541e4fb7bc010e7e682b80737ad0987a02c
ofirl <ofirl24@gmail.com>
2017-02-17 14:36:54 +0200

    fixes for thread transfer?

---
+++ src/main/java/interface_objects/DatabaseHandlerTest.java
@@ -0,0 +1,108 @@
+package interface_objects;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Ofir on 2/17/2017.
+ */
+public class DatabaseHandlerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void transferQuery() throws Exception {
+
+    }
+
+    @Test
+    public void receiveQuery() throws Exception {
+
+    }
+
+    @Test
+    public void responseEnqueue() throws Exception {
+
+    }
+
+    @Test
+    public void addResponse() throws Exception {
+
+    }
+
+    @Test
+    public void responseDequeue() throws Exception {
+
+    }
+
+    @Test
+    public void waitForResponse() throws Exception {
+
+    }
+
+    @Test
+    public void addQuery() throws Exception {
+
+    }
+
+    @Test
+    public void requestAction() throws Exception {
+
+    }
+
+    @Test
+    public void requestResponse() throws Exception {
+
+    }
+
+    @Test
+    public void getNextQueryId() throws Exception {
+
+    }
+
+    @Test
+    public void getResultSetColumns() throws Exception {
+
+    }
+
+    @Test
+    public void formatFilters() throws Exception {
+
+    }
+
+    @Test
+    public void getTableElements() throws Exception {
+
+    }
+
+    @Test
+    public void getTableElements1() throws Exception {
+
+    }
+
+    @Test
+    public void getTableElements2() throws Exception {
+
+    }
+
+    @Test
+    public void getTableElements3() throws Exception {
+
+    }
+
+    @Test
+    public void checkElementsExistence() throws Exception {
+
+    }
+
+}
